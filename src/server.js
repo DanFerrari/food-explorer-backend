6c0c370f288b61cd24606e2b1f982e15ed2265f1
DanFerrari <danieldasilvaferrari@gmail.com>
2023-08-02 23:25:33 -0300

    Add os diretórios e criado os arquivos de rotas

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log("Hello Word");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
